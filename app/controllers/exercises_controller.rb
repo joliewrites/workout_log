574f0107ed539a71f5590c5f1114351da146c8fb
Jolie Adam <as.jolie3@gmail.com>
2020-07-20 20:23:22 -0400

    controller tweak to nest exercises into workouts

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,8 +1,13 @@
 class ExercisesController < ApplicationController
   def create
     @workout = Workout.find(params[:workout_id])
-    @exercise = @workout.exercises.create(params[:workout.permit(:name, :sets, :reps])
+    @exercise = @workout.exercises.create(exercise_params)
 
     redirect_to workout_path(@workout)
+  end
+
+  private
+  def exercise_params
+    params.require(:exercise).permit(:name, :sets, :reps)
   end
 end
